4f2d8cabd673ddfb473a415cbc85cd4a1e82a6f4
fanghailong <fanghailong@Mac.local>
2016-04-11 09:07:25 +0800

    modify Note

--- hlcore-android/src/main/java/com/hlf/core/IWatcher.java
+++ hlcore-android/src/main/java/com/hlf/core/IWatcher.java
@@ -2,7 +2,7 @@ package com.hlf.core;
 
 /**
  * Created by fanghailong on 16/4/11.
- * IWatcher是对Action对象观察着的一个抽象，是处理Action的具体场景；
+ * IWatcher是对Action对象观察者的一个抽象，是处理Action的具体场景；
  */
 public interface IWatcher {
 
